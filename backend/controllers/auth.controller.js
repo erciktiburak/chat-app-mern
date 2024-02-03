a7129cc99ef9eb97114503b3491e4e197ebf97f6
Burak Erçıktı <burakercikti@outlook.com>
2024-02-03 11:33:28 +0300

    sign-up endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,47 @@
+import User from "../models/user.model";
+
 export const signup = async(req, res) => {
     try {
         const {fullName, username, password, confirmPassword, gender} = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({message: "Passwords do not match"});
+        }
+
+        const user = await User.findOne({username});
+
+        if(user){
+            return res.status(400).json({message: "User already exists"});
+        }
+
+        // Hash the password
+
+        //https://avatar-placeholder.iran.liara.run/
+
+        const boyProfilePicture = "https://avatar.iran.liara.run/public/boy?username${username}";
+        const girlProfilePicture = "https://avatar.iran.liara.run/public/girl?username${username}";
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePicture: gender === "male" ? boyProfilePicture : girlProfilePicture
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePicture: newUser.profilePicture
+
+        });
+
     } catch (error) {
-        console.log(error);
+        console.log("Error on signup controller", error.message)
+        res.status(500).json({error: "Internal Server Error"});
     }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,18 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes)
+
+
+/* app.get("/", (req, res) => {
+    res.send("Hello World!");
+}); */
 
 app.listen(PORT, () => {
     connectToMongoDB();
